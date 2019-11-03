4af5b5a46d571953848445c3e716148615a15d8a
Per Orest <per.orest@outlook.com>
2019-11-03 02:42:23 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NBSTimeReporting.Models.SettingModels;
+using NBSTimeReporting.Models.DataModels;
+
+namespace NBSTimeReporting.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<NBSTimeReporting.Models.SettingModels.PersonRole> PersonRole { get; set; }
+        public DbSet<NBSTimeReporting.Models.SettingModels.PersonStatus> PersonStatus { get; set; }
+        public DbSet<NBSTimeReporting.Models.SettingModels.PersonType> PersonType { get; set; }        
+        public DbSet<NBSTimeReporting.Models.SettingModels.SiteRole> SiteRole { get; set; }
+        public DbSet<NBSTimeReporting.Models.SettingModels.SiteStatus> SiteStatus { get; set; }
+        public DbSet<NBSTimeReporting.Models.SettingModels.SiteType> SiteType { get; set; }
+        public DbSet<NBSTimeReporting.Models.DataModels.Person> Person { get; set; }
+        public DbSet<NBSTimeReporting.Models.DataModels.Company> Company { get; set; }
+        public DbSet<NBSTimeReporting.Models.SettingModels.CompanyRole> CompanyRole { get; set; }
+        public DbSet<NBSTimeReporting.Models.SettingModels.CompanyStatus> CompanyStatus { get; set; }
+        public DbSet<NBSTimeReporting.Models.SettingModels.CompanyType> CompanyType { get; set; }
+        public DbSet<NBSTimeReporting.Models.DataModels.Site> Site { get; set; }
+    }
+}
---
+++ Models/SettingModels/CompanyType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBSTimeReporting.Models.SettingModels
+{
+    public class CompanyType
+    {
+        public int Id { get; set; }
+
+        public string CompanyTypeName { get; set; }
+    }
+}
